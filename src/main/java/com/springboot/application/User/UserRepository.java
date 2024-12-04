f877ed0ae459bf3b42ee138338806bba0f9871c2
gaiusYan <gaiusyanbena11@gmail.com>
2024-12-04 12:03:50 +0100

    EmployModel

--- src/main/java/com/springboot/application/User/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.application.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserApplication,Long>{
-
-}
